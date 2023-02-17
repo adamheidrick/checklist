438ddd0541b61119c89355e22279287fcb4f9a26
adamheidrick <adamheidrick@gmail.com>
2023-02-17 15:16:56 +0100

    I see. Odoo is all about modules and inheritence. Implemented CRM inheritence and added an empty page to the Notebook

--- __manifest__.py
+++ __manifest__.py
@@ -23,13 +23,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['crm'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+        'views/checklist.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/models.py
+++ models/models.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class checklist(models.Model):
-#     _name = 'checklist.checklist'
-#     _description = 'checklist.checklist'
+class CheckList(models.Model):
+    _inherit = 'crm.lead'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
